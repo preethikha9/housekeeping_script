35ec46caa70f5421e5b158a24bea3407f736900a
Preethikha S <spreethikha@vmware.com>
2020-08-19 21:28:05 +0530

    path package

--- path_package.py
+++ path_package.py
@@ -1,3 +1,24 @@
 import os
 for root in os.walk("./path_check", topdown=False):
    print(root)
+
+
+''''
+Structure:
+
+path_check
+    |__check1
+    |     |______subcheck1
+    |     |______subcheck2
+    |__check2
+    |__check3
+'''
+
+''' Output:
+('./path_check/check1/subcheck1', [], [])
+('./path_check/check1/subcheck2', [], [])
+('./path_check/check1', ['subcheck1', 'subcheck2'], [])
+('./path_check/check2/subcheck3', [], [])
+('./path_check/check2', ['subcheck3'], [])
+('./path_check/check3', [], [])
+('./path_check', ['check1', 'check2', 'check3'], []) '''
